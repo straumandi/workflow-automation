b78b02f1aa203b58805380fb259872108dcfdb6a
Andreas Armanious <52195696+straumandi@users.noreply.github.com>
2024-10-22 14:42:39 +0200

    Add basic java calculator files

---
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -0,0 +1,7 @@
+package at.fhj.msd;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/at/fhj/msd/CalculatorTest.java
@@ -0,0 +1,14 @@
+package at.fhj.msd;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+}
